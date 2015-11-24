b0ec1c3baa784998e98283d0d238005c05f6c621
Giulio Roggero <giulio.roggero@agilereloaded.it>
2015-11-24 16:19:59 +0100

    first commit

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.get('/', function (req, res) {
       res.send('Hello World!');
 });
 
-var server = app.listen(80, function () {
-      var host = server.address().address;
-        var port = server.address().port;
+var p = process.env.PORT || 1337;
+var server = app.listen(p, function () {
+var host = server.address().address;
+var port = server.address().port;
 
-          console.log('Example app listening at http://%s:%s', host, port);
+   console.log('Example app listening at http://%s:%s', host, port);
 });
